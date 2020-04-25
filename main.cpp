069b36af53165e5e7f5deaf92834d2b0a831a2de
Peter Pham <peterpham86@gmail.com>
2020-04-25 15:13:11 -0700

    modify code to split file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <math.h>
-#include <fstream>
 
 #define sampling_rate 500000000;
+#define minSymbol 33 // maps to !
+#define maxSymbol 47 // maps to /
 
 int main(int argc, char *argv[]) {
   printf("Opening file:  %s\n", argv[1]);
@@ -13,10 +16,11 @@ int main(int argc, char *argv[]) {
 
   // Input stream
   FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL) exit(EXIT_FAILURE);
+  if (fp == NULL)
+    exit(EXIT_FAILURE);
 
   // Ouput stream
-  std::ofstream outFile("output.txt", std::ofstream::out);
+  // std::ofstream outFile("output.txt", std::ofstream::out);
   char *line = NULL;
   size_t len = 0;
   int i = 0;
@@ -29,89 +33,97 @@ int main(int argc, char *argv[]) {
   int index = 0;
   bool doneVarRead = false;
   bool foundVar = false;
-  long int currFrame = 0;
-  long int prevFrame = 0;
+
   int startTime = 0;
   int endTime = 0;
-  double endTimeDouble= 0.0;
+  double endTimeDouble = 0.0;
   char header;
-  char dataMap[256];
-  for (i = 0; i < 256; i++) dataMap[i] = 0;
-
+  char inputSymbol[2];
+  int symbolIdx;
+  long int currFrame;
+  long int symbolPrevFrame[256];
+  // symbol can have different previous array
+  std::ofstream *filePtr[256];
+  for (i = 0; i < 256; i++) {
+    filePtr[i] = NULL;
+    symbolPrevFrame[i] = 0;
+  }
   while ((getline(&line, &len, fp)) != -1) {
     // using printf() in all tests for consistency
     // printf("%s", line);
     header = line[0];
     switch (header) {
-      case '$':
-        if (memcmp(line, "$var wire", 8) == 0) {
-          if (foundVar == false && !doneVarRead) {
-            // first time we detected a $varwire
-            outFile << "TIMESTAMP,";
-          }
-          // advance line by 12 characters to get rid of "var wire X "
-          line = line + 12;
-          count = strlen(line);
+    case '$':
+      if (memcmp(line, "$var wire", 8) == 0) {
+        /*if (foundVar == false && !doneVarRead) {
+          // first time we detected a $varwire
+          //outFile << "TIMESTAMP,";
+        }*/
+        // advance line by 12 characters to get rid of "var wire X "
+        line = line + 12;
+        count = strlen(line);
 
-          // get rid of the $end so we only get the variable name
-          line[count - 6] = 0;
-          outFile << line << ",";  // print out variable name
-          foundVar = true;
+        // get rid of the $end so we only get the variable name
+        line[count - 6] = 0;
+        // grab the input symbol to be used as part of the output file name
+        inputSymbol[0] = line[0];
+        inputSymbol[1] = 0;
+        symbolIdx = (int)inputSymbol[0];
+        // create new output stream per found symbol
+        std::string fileName = "output_" + std::string(inputSymbol) + ".txt";
+        std::cout << "index for: " << inputSymbol << " is " << symbolIdx;
+        std::cout << "creating output file: " << fileName << "\n";
+        filePtr[symbolIdx] = new std::ofstream(fileName, std::ofstream::out);
+        *filePtr[symbolIdx] << "TIMESTAMP," << line
+                            << "\n"; // print out variable name
+        foundVar = true;
 
-        } else {
-          // we did not match a $varwire but previously did
-          // so now we must be done scanning for var name
-          if (foundVar == true) {
-            doneVarRead = true;
-            outFile << "\n";
-          }
-          foundVar = false;
+      } else {
+        // we did not match a $varwire but previously did
+        // so now we must be done scanning for var name
+        if (foundVar == true) {
+          doneVarRead = true;
+          // outFile << "\n";
         }
-        break;
-      case '#':
-        sscanf(line + 1, "%ld", &currFrame);
-        if (prevFrame == 0) {
-          // printf("Initial frame: %ld\n",currFrame);
-          prevFrame = currFrame;
-        }
+        foundVar = false;
+      }
+      break;
+    case '#':
+      sscanf(line + 1, "%ld", &currFrame);
+      break;
+    default:
+      // We got data so just store it in our dataMap buffer
+      // we will print out the dataMap buffer once we get to the next #
+      data = line[0];
+      index = (int)line[1];
+      // dataMap[index] = data;
 
-        // Compute endTime
-        endTimeDouble =
-                    double(currFrame - prevFrame) / (double)1000000000 * sampling_rate;
-        endTime = round(endTimeDouble);
-        
+      if (symbolPrevFrame[index] == 0) {
+        // printf("Initial frame: %ld\n",currFrame);
+        symbolPrevFrame[index] = currFrame;
+      }
 
-        // do {
-        // This do loop is simply to interpolate between the
-        // time stamps
-        if (endTime > 0) {
-          outFile << endTime << ",";
-          for (j = 32; j < 49; j++) {
-            // print out all data map if we have data for it
-            if (dataMap[j] != 0) {
-              outFile << dataMap[j] << ",";
-            }
-          }
-        }
-
-        outFile << "\n";
-        //} while (step < endTime);
-        prevFrame = currFrame;
-
-        step = endTime;
-        break;
-      default:
-        // We got data so just store it in our dataMap buffer
-        // we will print out the dataMap buffer once we get to the next #
-        data = line[0];
-        index = (int)line[1];
-        dataMap[index] = data;
+      // Compute endTime
+      endTimeDouble = double(currFrame - symbolPrevFrame[index]) /
+                      (double)1000000000 * sampling_rate;
+      endTime = round(endTimeDouble);
+      if (endTime > 0)
+        *filePtr[index] << endTime << "," << data << "\n";
+      // store the timestamp for the scanned timeframe
+      symbolPrevFrame[index] = currFrame;
     }
   }
 
+  // close input file
   fclose(fp);
-  outFile.flush();
-  outFile.close();
+
+  // close output files
+  for (j = minSymbol; j < maxSymbol; j++) {
+    if (filePtr[j] != NULL) {
+      filePtr[j]->flush();
+      filePtr[j]->close();
+    }
+  }
 
   time_t endTs = clock();
   double elapsed = (double)(endTs - startTs) / CLOCKS_PER_SEC;
